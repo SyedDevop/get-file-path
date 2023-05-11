23c1408c26844208e0f0eca597a9f7666f0ae028
syed Devop <syeds.devops007@gmail.com>
2023-05-11 21:30:52 +0530

    testing clap

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 // use rfd::FileDialog;
-use std::env;
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:#?}", args)
-    // let files = FileDialog::new().pick_folder();
-    // if files.is_none() {
-    //     print!("User did not choose")
-    // } else {
-    //     println!("The user choose: {:#?}", files);
-    // }
-    
-}
 
-fn parse_config(args: &[String]) -> &str {
-    let query = &args[1];
-    &query
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// File Select type (file || folder)
+    #[arg(short, long, get_possible_values= &["file", "folder"])]
+    select: String,
+}
+fn main() {
+    let args = Args::parse();
+
+    for _ in 0..10 {
+        println!("Hello {}!", args.select)
+    } // let files = FileDialog::new().pick_folder();
+      // if files.is_none() {
+      //     print!("User did not choose")
+      // } else {
+      //     println!("The user choose: {:#?}", files);
+      // }
 }
